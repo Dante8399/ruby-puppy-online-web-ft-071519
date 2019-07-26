71bc6a9e3dcc7c3c862869cf97dd0a23b7ffaaa3
Dante Spencer <spencerdantel@gmail.com>
2019-07-26 14:28:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Add your code here
+class Dog
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.print_all
+    @@all.each do |element| 
+      @@all << element
+      puts @@all
+  end
+end
